a3cc109bff85006bb6d3fe95fa8272e0ba6c266b
Михаил <misha13022008@gmail.com>
2023-09-30 15:04:26 +0300

    crud

---
+++ TaskList4/Migrations/20230927102826_init.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TaskList4.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Archived = table.Column<bool>(type: "boolean", nullable: false),
+                    Finished = table.Column<bool>(type: "boolean", nullable: false),
+                    PersonId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_PersonId",
+                table: "Tasks",
+                column: "PersonId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ TaskList4/Program.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+var person = new Person
+{
+    Id = 123,
+    Name = "Nick",
+    Password = "qwerty",
+    Tasks = new List<Task>(),
+};
+var task = new Task
+{
+    Title = "Title",
+    Description = "Dsc",
+    Archived = false,
+    Finished = false
+};
+
+new AppContext().AddPerson(person);
+new AppContext().AddTask(task, person);
+
+Console.WriteLine("Hello, World!");
+
+
+public class AppContext : DbContext
+{
+    private DbSet<Person> Persons { get; set; } = null!;
+    private DbSet<Task> Tasks { get; set; } = null!;
+
+    public void AddPerson(Person p)
+    {
+        Persons.Add(p);
+        SaveChanges();
+    }
+
+    public void RemovePerson(Person p)
+    {
+        Persons.Remove(p);
+        SaveChanges();
+    }
+
+    public void UpdatePerson(Person p)
+    {
+        Persons.Update(p);
+        SaveChanges();
+    }
+
+    public void AddTask(Task t, Person p)
+    {
+        Tasks.Add(t);
+        p.Tasks.Add(t);
+        UpdatePerson(p);
+    }
+
+    public void RemoveTask(Task t, Person p)
+    {
+        Tasks.Remove(t);
+        p.Tasks.Remove(t);
+        UpdatePerson(p);
+    }
+
+    public void UpdateTask(Task t, Person p)
+    {
+        Tasks.Update(t);
+        SaveChanges();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=testDb;username=postgres;Password=Qwary123");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
+
+
+public class Person : Entity
+{
+    public required string Name { get; set; }
+    public required string Password { get; set; }
+    public required List<Task> Tasks { get; set; }
+}
+
+public class Task : Entity
+{
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required bool Archived { get; set; }
+    public required bool Finished { get; set; }
+}
+
+public abstract class Entity
+{
+    public int Id { get; set; }
+}
